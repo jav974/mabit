e645cf273a9159a771fe34705030967efd61e262
Jeremy <jeremy@cedric-pc.(none)>
2013-06-12 13:59:11 +0200

    Added representation for octal and hexadecimal

--- mabit_stream.hpp
+++ mabit_stream.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <utility>
 #include "mabit_traits.hpp"
 #include "mabit.hpp"
 
@@ -31,11 +32,11 @@ namespace Mabit
 	case BIN:
 	  return to_bin(nb, sep);
 	case OCT:
-	  return to_oct(nb, sep);
+	  return to_oct(nb, '.');
 	case DEC:
 	  return to_dec(nb, sep);
 	case HEX:
-	  return to_hex(nb, sep);
+	  return to_hex(nb, '.');
 	}
       return to_bin(nb, sep);
     }
@@ -85,7 +86,11 @@ namespace Mabit
     
     static std::string	to_oct(const mabit_t& nb, const char sep)
     {
+      std::vector<char>	tmp;
       std::string	ret;
+
+      bin_to_base(nb, OCT, tmp);
+      build_str(ret, tmp, sep, nb._sign, 2, OCT);
 
       return ret;
     }
@@ -95,48 +100,72 @@ namespace Mabit
       std::vector<char>	tmp;
       std::string	ret;
 
-      tmp.push_back('0');
-	  
-      word_t		simulated_abs;
-      
-      for (msize_t words = nb.used_words(); words > 0; --words)
-	{
-	  simulated_abs = nb.simulate_abs(words - 1);
-	  
-	  for (msize_t i = 0; i < nb._set.BITS_IN_WORD; ++i)
-	    {
-	      mult_by(tmp, 2, Mabit::DEC);
-	      if (nb.get_bit(simulated_abs, nb._set.BITS_IN_WORD - 1 - i))
-		add_one(tmp, 0, Mabit::DEC);
-	    }
-	}
+      bin_to_base(nb, DEC, tmp);
+      build_str(ret, tmp, sep, nb._sign, 3, DEC);
 
-      ret.resize(tmp.size() + tmp.size() / 3);
-
-      for (size_t i = 0; i < tmp.size(); ++i)
-	{
-	  ret += tmp[i];
-	  if (sep && (i && (i % 3) == 2 && i != tmp.size() - 1))
-	    ret += sep;
-	}
-    
-      if (!nb._sign)
-	ret += '-';
-      
-      std::reverse(ret.begin(), ret.end());
       return ret;
     }
 
     static std::string	to_hex(const mabit_t& nb, const char sep)
     {
+      std::vector<char>	tmp;
       std::string	ret;
+
+      bin_to_base(nb, HEX, tmp);
+      build_str(ret, tmp, sep, nb._sign, 2, HEX);
 
       return ret;
     }
 
+    static void		bin_to_base(const mabit_t& data, const base_t base, std::vector<char>& ret)
+    {
+      word_t		abs;
+
+      ret.push_back(0);
+
+      for (msize_t i = data.used_words(); i > 0; --i)
+	{
+	  abs = data.simulate_abs(i - 1);
+
+	  for (msize_t j = 0; j < data._set.BITS_IN_WORD; ++j)
+	    {
+	      mult_by(ret, 2, base);
+	      if (data.get_bit(abs, data._set.BITS_IN_WORD - 1 - j))
+		add_one(ret, 0, base);
+	    }
+	}
+    }
+
+    static void		build_str
+    (
+     std::string& ret,
+     const std::vector<char>& nb,
+     const char sep,
+     const bool sign,
+     const size_t delimit_pos,
+     const base_t base
+     )
+    {
+      ret.resize(nb.size() + nb.size() / delimit_pos);
+
+      for (size_t i = 0; i < nb.size(); ++i)
+	{
+	  char a = nb[i];
+
+	  ret += (base != HEX ? a + '0' : (a >= 10 ? (a - 10) + 'A' : a + '0'));
+
+	  if (sep && (i && (i % delimit_pos) == (delimit_pos - 1) && i != nb.size() - 1))
+	    ret += sep;
+	}
+      if (!sign)
+	ret += '-';
+
+      std::reverse(ret.begin(), ret.end());
+    }
+
     static void		add_one(std::vector<char>& nb, size_t i, const base_t base)
     {
-      const char	limit = (static_cast<int>(base) - 1) + '0';
+      const char	limit = (static_cast<int>(base) - 1);
 
       for (; i < nb.size(); ++i)
 	{
@@ -145,9 +174,9 @@ namespace Mabit
 	      nb[i] += 1;
 	      return ;
 	    }
-	  nb[i] = '0';
+	  nb[i] = 0;
 	}
-      nb.push_back('1');
+      nb.push_back(1);
     }
 
     static void		mult_by(std::vector<char>& nb, unsigned int mult_by, const base_t base)
@@ -157,10 +186,10 @@ namespace Mabit
 
       for (size_t i = 0; i < size; ++i)
 	{
-	  unsigned int	add_how_much = (nb[i] - '0') * mult_by;
+	  unsigned int	add_how_much = nb[i] * mult_by;
 
 	  if (!add_how_much && i < size - 1)
-	    ret.push_back('0');
+	    ret.push_back(0);
 
 	  for (; add_how_much > 0; --add_how_much)
 	    add_one(ret, i, base);
